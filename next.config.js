b6f2645906253e0ae5f24cb49b1c8e31b472a596
justmadhu <madhu.muthukumar@gmail.com>
2025-05-04 11:01:16 -0400

    Delete all

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-const repo = '<YOUR_REPO>'; // set your repo name
-module.exports = {
-  output: 'export',
-  images: { unoptimized: true },
-  basePath: isProd ? `/${repo}` : '',
-  assetPrefix: isProd ? `/${repo}/` : '',
-};
